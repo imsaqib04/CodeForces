9d86b9807358ec1fcab4f236c11100dc88364c34
saqib <saqibansari2011@gmail.com>
2026-01-13 13:28:25 +0530

    solved

---
+++ miscellaneous/src/PashmakAndFlowers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PashmakAndFlowers {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+
+            char x = s.charAt(0);
+            char y = s.charAt(1);
+
+            int index = (x - 'a') * 25
+                    + (y - 'a')
+                    - (y > x ? 1 : 0)
+                    + 1;
+
+            sb.append(index).append('\n'); // store
+        }
+
+        // 🔥 ek hi baar print
+        System.out.print(sb.toString());
+    }
+}
