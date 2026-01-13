4b453dd4add9bb5fa0ecc1694e7b14e618deecdd
saqib <saqibansari2011@gmail.com>
2026-01-13 15:42:52 +0530

    name changed

--- miscellaneous/src/Dictionary.java
+++ miscellaneous/src/Dictionary.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PashmakAndFlowers {
+public class Dictionary {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
